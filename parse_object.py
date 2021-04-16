e4622b99b9340a046a1f1c01d56134fa8a7c664c
sabinachang <sabchang1@gmail.com>
2021-04-16 12:54:43 +0800

    Refactor parser.py
    - Extract each project's info in parse_object
    - Extract steps in parse() to appropriate methods

---
+++ parse_object.py
@@ -0,0 +1,16 @@
+class ParseObject:
+    def __init__(self,import_start, ignore_path, file_end, project_name, default_pkg):
+        # dict of pacakge to list of nodes  
+        self.structure = {}
+
+        # dict of node to list of dependent nodes
+        self.dependency = {}
+
+        self.ignore_path = ignore_path
+
+        self.valid_import_start = import_start
+        self.valid_file_end = file_end
+        self.project_name = project_name
+        self.default_pkg = default_pkg
+        self.pkgs_file_suffix = "_pkgs.txt"
+        self.repr_file_suffix = "_repr.txt"
--- parser.py
+++ parser.py
@@ -2,74 +2,69 @@ import pkg_resources
 from os import walk
 from os.path import join
 from javalang import parse
+from parse_object import ParseObject
 
 class Parser:
-    def __init__(self, import_start, base, file_end, project_name):
-        
-        # dict of pacakge to list of nodes
-        self.structure = {}
+    def __init__(self, parse_object):
+        self.po = parse_object
 
-        # dict of node to list of dependent nodes
-        self.dependency = {}
-
-        self.base = base
-
-        self.valid_import_start = import_start
-        self.valid_file_end = file_end
-        self.project_name = project_name
-
-    def main(self):
-        pkgs_file = self.project_name + "_pkgs.txt"
-        packages = []
-
-        with open(pkgs_file, "r") as pf:
-            lines = pf.readlines()
-            for l in lines:
-                packages.append(l.replace("\n",""))
+    def parse(self):
+        packages = self.__read_pkgs_file()
             
         for package in packages:
             # package name as key
-            key = package.replace(self.base,"",1)
-            self.structure[key] = []
+            key = package.replace(self.po.ignore_path,"",1)
+            self.po.structure[key] = []
 
             # list of nodes in package as value. For nodes in subpackage, 
             # node name includes the subpackage path to avoid duplicate names across subpackages
             # ex: in command, one node name equals /commands/CommandHistory.java
             for root, _, files in walk(package):
                 for f in files:
-                    if not f.endswith(self.valid_file_end):
+                    if not f.endswith(self.po.valid_file_end):
                         continue
                     node = join(root,f)
-                    d = self.get_dependencies(node)
+                    d = self.__get_dependencies(node)
 
-                    node = node.replace(self.base,"",1)
-                    self.dependency[node] = d
-                    self.structure[key].append(node)
+                    node = node.replace(self.po.ignore_path,"",1)
+                    self.po.dependency[node] = d
+                    self.po.structure[key].append(node)
 
-        
-        repr_file = self.project_name + "_repr.txt"
+        self.__create_text_representation()
+    
+    def __read_pkgs_file(self):
+        pkgs_file = self.po.project_name + self.po.pkgs_file_suffix
+        packages = []
+
+        with open(pkgs_file, "r") as pf:
+            lines = pf.readlines()
+            for l in lines:
+                packages.append(l.replace("\n",""))
+
+        return packages
+
+    def __create_text_representation(self):
+        repr_file = self.po.project_name + self.po.repr_file_suffix
         with open(repr_file, "w") as rf:
             
-            for key, values in self.structure.items():
+            for key, values in self.po.structure.items():
                 rf.write(key + "\n")
                 rf.write(", ".join(values))
                 rf.write("\n\n")
 
                 for v in values:
                     rf.write(v + ": ")
-                    rf.write(", ".join(self.dependency[v])+ "\n")
+                    rf.write(", ".join(self.po.dependency[v])+ "\n")
                 rf.write("\n\n")
-    
 
-
-    def get_dependencies(self, node):
+    def __get_dependencies(self, node):
 
         source = pkg_resources.resource_string(__name__, node)
         ast = parse.parse(source)
 
         dependencies = []
         for imp in ast.imports:
-            if not imp.path.startswith(self.valid_import_start):
+            if not imp.path.startswith(self.po.valid_import_start):
                 continue
             dependencies.append(imp.path.replace(".", "/"))
 
@@ -77,7 +72,6 @@ class Parser:
 
 
 if __name__ == '__main__':
-
     # for DesignPatterns
-    p = Parser('patterns', 'DesignPatterns-master/src/', '.java','designPattern')
-    p.main()
+    obj = ParseObject('patterns', 'DesignPatterns-master/src/', '.java','designPattern', "")
+    Parser(obj).parse()
