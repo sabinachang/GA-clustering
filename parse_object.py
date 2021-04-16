eec09ba4753a2cc5ae7a7a69860bcb897558106b
sabinachang <sabchang1@gmail.com>
2021-04-16 15:08:25 +0800

    Create setters for parseObject
    - Allow params to be optional
    - Reduce constructor size

--- parse_object.py
+++ parse_object.py
@@ -1,16 +1,34 @@
 class ParseObject:
-    def __init__(self,import_start, ignore_path, file_end, project_name, default_pkg):
+    def __init__(self):
         # dict of pacakge to list of nodes  
         self.structure = {}
 
         # dict of node to list of dependent nodes
         self.dependency = {}
 
-        self.ignore_path = ignore_path
+        self.ignore_path = ""
+        self.valid_import_start = ""
+        self.valid_file_end = ""
+        self.text_repr_name = ""
+        self.default_pkg = ""
+        self.strct_file_path = ""
 
-        self.valid_import_start = import_start
-        self.valid_file_end = file_end
-        self.project_name = project_name
-        self.default_pkg = default_pkg
-        self.strct_file_suffix = "_strct.txt"
-        self.repr_file_suffix = "_repr.txt"
+        self.output_dir_path = "./output"
+    
+    def set_strct_file_path(self, path):
+        self.strct_file_path = path
+
+    def set_path_to_ignore(self,path):
+        self.ignore_path = path
+    
+    def set_valid_import_start(self, start):
+        self.valid_import_start = start
+    
+    def set_default_pkg_name(self, pkg):
+        self.default_pkg = pkg
+
+    def set_valid_file_end(self,suffix):
+        self.valid_file_end = suffix
+    
+    def set_text_repr_name(self,name):
+        self.text_repr_name = name
